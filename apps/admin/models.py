ad83112ea040d0337b5c01af7332ed62fe20a174
Abdurasul Ergashev <abdurasul15.10.1998@gmail.com>
2025-08-08 01:38:57 +0500

    admin done

--- apps/admin/models.py
+++
@@ -1,7 +0,0 @@
-messages_query = """
-                 CREATE TABLE IF NOT EXISTS admin
-                 (
-                     id         SERIAL PRIMARY KEY,
-                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                 )
-                 """
